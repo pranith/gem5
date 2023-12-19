fddef80b43c996be77c07db772d2f68997ddc821
Pranith Kumar <bobby.prani@gmail.com>
2023-12-18 23:01:35 -0500

    cpu-o3: store_set: Use the generic cache library
    Change-Id: I18ddad2346b82423c01ef9117a5d3d85f7039be7

--- src/cpu/o3/mem_dep_unit.cc
+++ src/cpu/o3/mem_dep_unit.cc
@@ -56,8 +56,8 @@ MemDepUnit::MemDepUnit() : iqPtr(NULL), stats(nullptr) {}
 
 MemDepUnit::MemDepUnit(const BaseO3CPUParams &params)
     : _name(params.name + ".memdepunit"),
-      depPred(params.store_set_clear_period, params.SSITSize,
-              params.LFSTSize),
+      depPred(params.store_set_clear_period, params.SSITSize, params.SSITAssoc,
+              params.SSITReplPolicy, params.LFSTSize),
       iqPtr(NULL),
       stats(nullptr)
 {
@@ -97,7 +97,7 @@ MemDepUnit::init(const BaseO3CPUParams &params, ThreadID tid, CPU *cpu)
     id = tid;
 
     depPred.init(params.store_set_clear_period, params.SSITSize,
-            params.LFSTSize);
+                 params.SSITAssoc, params.SSITReplPolicy, params.LFSTSize);
 
     std::string stats_group_name = csprintf("MemDepUnit__%i", tid);
     cpu->addStatGroup(stats_group_name.c_str(), &stats);
