d1b9ed350051ef592ef3b3f64cdd59ead7540eae
Pranith Kumar <bobby.prani@gmail.com>
2024-03-28 17:26:54 -0400

    wip

--- util/run.sh
+++ util/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-gdb --args ./build/ARM/gem5.debug -d debug --debug-flags=O3CPUAll configs/example/arm/rancho_o3.py test --cpu rancho --mem-size 8GB
+#gdb --args ./build/ARM/gem5.debug -d debug --debug-flags=O3CPUAll configs/example/arm/rancho_o3.py test --cpu rancho --mem-size 8GB
+./build/ARM/gem5.opt -d debug --debug-flags=O3CPUAll configs/example/arm/rancho_o3.py test --cpu rancho --mem-size 8GB
