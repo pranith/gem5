dhschall/gem5-fdp

cpu-o3: Allow non-serializing update of MiscRegClass registers
With this commit we finally track some MiscRegs with the
o3 Scoreboard, allowing some of the to be speculatively
updated. This needs to be enabled on the ISA side
(see incoming patches)

Signed-off-by: Giacomo Travaglini <giacomo.travaglini.arm.com>
Change-Id: I00aa08b414364123becb5d2faf8eba3da902934a

src/cpu/o3/rename.cc
@@ -695,6 +695,12 @@ Rename::renameInsts(ThreadID tid)
             break;
         }
 
+        // If we detect a WaW conflict for MiscRegs, we tag
+        // the instruction as SerializeBefore to avoid the conflict
+        if (!inst->isSerializeAfter() && inst->numDestRegs(MiscRegClass)) {
+            setSerializeBefore(inst, tid);
+        }
+
         // Handle serializeAfter/serializeBefore instructions.
         // serializeAfter marks the next instruction as serializeBefore.
         // serializeBefore makes the instruction wait in rename until the ROB
@@ -960,6 +966,15 @@ Rename::doSquash(const InstSeqNum &squashed_seq_num, ThreadID tid)
             freeingInProgress[tid].push_back(hb_it->newPhysReg);
         }
 
+        // mark the speculative register as ready in the scoreboard as
+        // the producing instruction is being squashed.
+        // This is not necessary for renameable register as the physical
+        // register is unreachable until it gets recycled as a new destination
+        // register (it will be marked as busy anyway next time it gets used).
+        // This is instead required for fixed mapping registers which could
+        // be rereferenced as a source registers after the squash
+        scoreboard->setReg(hb_it->newPhysReg);
+
         // Notify potential listeners that the register mapping needs to be
         // removed because the instruction it was mapped to got squashed. Note
         // that this is done before hb_it is incremented.
@@ -1152,6 +1167,38 @@ Rename::renameDestRegs(const DynInstPtr &inst, ThreadID tid)
                             rename_result.second);
 
         ++stats.renamedOperands;
+    }
+}
+
+void
+Rename::setSerializeBefore(DynInstPtr &inst, ThreadID tid)
+{
+    // If the destination register is already being produced, we mark the
+    // instruction as serialize before, stalling it at rename until
+    // the ROB is empty. This is an expensive way to deal with a WAW
+    // conflict. Even if the instruction is marked as non speculative
+    // and will only execute at commit time, the dependency logic
+    // (called at dispatch time)
+    // is not capable of handling two producers of the same register
+    // at the same time.
+    unsigned num_dest_regs = inst->numDestRegs();
+    for (int dest_idx = 0; dest_idx < num_dest_regs; dest_idx++) {
+
+        gem5::ThreadContext *tc = inst->tcBase();
+        UnifiedRenameMap *map = renameMap[tid];
+        auto *isa = tc->getIsaPtr();
+
+        const RegId &dest_reg = inst->destRegIdx(dest_idx);
+        const RegId flat_reg = dest_reg.flatten(*isa);
+        PhysRegIdPtr phys_reg = map->lookup(flat_reg);
+
+        if (phys_reg->classValue() == MiscRegClass &&
+            !phys_reg->isAlwaysReady() &&
+            !scoreboard->getReg(map->lookup(flat_reg))) {
+
+            inst->setSerializeBefore();
+            return;
+        }
     }
 }
 

src/cpu/o3/rename.hh
@@ -255,6 +255,9 @@ class Rename
     /** Renames the destination registers of an instruction. */
     void renameDestRegs(const DynInstPtr &inst, ThreadID tid);
 
+    /** Should we SerializeBefore the current instruction */
+    void setSerializeBefore(DynInstPtr &inst, ThreadID tid);
+
     /** Calculates the number of free ROB entries for a specific thread. */
     int calcFreeROBEntries(ThreadID tid);
 

pranith/gem5

src/cpu/o3/rename.cc
@@ -695,6 +695,12 @@ Rename::renameInsts(ThreadID tid)
             break;
         }
 
+        // If we detect a WaW conflict for MiscRegs, we tag
+        // the instruction as SerializeBefore to avoid the conflict
+        if (!inst->isSerializeAfter() && inst->numDestRegs(MiscRegClass)) {
+            handleMiscRegWaW(inst, tid);
+        }
+
         // Handle serializeAfter/serializeBefore instructions.
         // serializeAfter marks the next instruction as serializeBefore.
         // serializeBefore makes the instruction wait in rename until the ROB
@@ -960,6 +966,15 @@ Rename::doSquash(const InstSeqNum &squashed_seq_num, ThreadID tid)
             freeingInProgress[tid].push_back(hb_it->newPhysReg);
         }
 
+        // mark the speculative register as ready in the scoreboard as
+        // the producing instruction is being squashed.
+        // This is not necessary for renameable register as the physical
+        // register is unreachable until it gets recycled as a new destination
+        // register (it will be marked as busy anyway next time it gets used).
+        // This is instead required for fixed mapping registers which could
+        // be rereferenced as a source registers after the squash
+        scoreboard->setReg(hb_it->newPhysReg);
+
         // Notify potential listeners that the register mapping needs to be
         // removed because the instruction it was mapped to got squashed. Note
         // that this is done before hb_it is incremented.
@@ -1152,6 +1167,38 @@ Rename::renameDestRegs(const DynInstPtr &inst, ThreadID tid)
                             rename_result.second);
 
         ++stats.renamedOperands;
+    }
+}
+
+void
+Rename::handleMiscRegWaW(DynInstPtr &inst, ThreadID tid)
+{
+    // If the destination register is already being produced, we mark the
+    // instruction as serialize before, stalling it at rename until
+    // the ROB is empty. This is an expensive way to deal with a WAW
+    // conflict. Even if the instruction is marked as non speculative
+    // and will only execute at commit time, the dependency logic
+    // (called at dispatch time)
+    // is not capable of handling two producers of the same register
+    // at the same time.
+    unsigned num_dest_regs = inst->numDestRegs();
+    for (int dest_idx = 0; dest_idx < num_dest_regs; dest_idx++) {
+
+        gem5::ThreadContext *tc = inst->tcBase();
+        UnifiedRenameMap *map = renameMap[tid];
+        auto *isa = tc->getIsaPtr();
+
+        const RegId &dest_reg = inst->destRegIdx(dest_idx);
+        const RegId flat_reg = dest_reg.flatten(*isa);
+        PhysRegIdPtr phys_reg = map->lookup(flat_reg);
+
+        if (phys_reg->classValue() == MiscRegClass &&
+            !phys_reg->isAlwaysReady() &&
+            !scoreboard->getReg(map->lookup(flat_reg))) {
+
+            inst->setSerializeBefore();
+            return;
+        }
     }
 }
 

src/cpu/o3/rename.hh
@@ -255,6 +255,9 @@ class Rename
     /** Renames the destination registers of an instruction. */
     void renameDestRegs(const DynInstPtr &inst, ThreadID tid);
 
+    /** Should we SerializeBefore the current instruction */
+    void handleMiscRegWaW(DynInstPtr &inst, ThreadID tid);
+
     /** Calculates the number of free ROB entries for a specific thread. */
     int calcFreeROBEntries(ThreadID tid);
 