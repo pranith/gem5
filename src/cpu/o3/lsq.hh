acdd75d67c69232b88117b44402033a47b390232
Pranith Kumar <bobby.prani@gmail.com>
2026-01-06 20:41:44 -0800

    misc: Add merge buffer

--- src/cpu/o3/lsq.hh
+++ src/cpu/o3/lsq.hh
@@ -852,6 +852,7 @@ class LSQ
      * to memory.
      */
     bool hasStoresToWB(ThreadID tid);
+    void forceMBDrain(ThreadID tid);
 
     /** Returns the number of stores a specific thread has to write back. */
     int numStoresToWB(ThreadID tid);
