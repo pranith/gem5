25fe38e6cf274d31aabd25a6bb324c29a3113000
Pranith Kumar <bobby.prani@gmail.com>
2026-01-23 21:58:23 +0000

    misc: Add load stall stats

--- src/cpu/o3/lsq_unit.cc
+++ src/cpu/o3/lsq_unit.cc
@@ -392,6 +392,8 @@ LSQUnit::LSQUnitStats::LSQUnitStats(statistics::Group *parent)
                "Sum of SQ occupancy during barrier stall cycles"),
       ADD_STAT(mbReleaseWaitCycles, statistics::units::Count::get(),
                "Cycles release MB entries waited for outstanding bytes"),
+      ADD_STAT(mbVersionLoadStallCycles, statistics::units::Count::get(),
+               "Cycles loads waited on older merge buffer versions"),
       ADD_STAT(barrierReschedulesLSQ, statistics::units::Count::get(),
                "Instructions rescheduled/replayed due to barrier in LSQ")
 {
@@ -2500,7 +2502,7 @@ LSQUnit::youngestMBVersion() const
 }
 
 bool
-LSQUnit::loadBlockedByMBVersion(uint64_t version) const
+LSQUnit::loadBlockedByMBVersion(uint64_t version)
 {
     if (!mergeBufferEnabled || !cpu->versioningEnabled()) {
         return false;
@@ -2511,7 +2513,12 @@ LSQUnit::loadBlockedByMBVersion(uint64_t version) const
         return false;
     }
 
-    return version > *youngest;
+    if (version > *youngest) {
+        stats.mbVersionLoadStallCycles++;
+        return true;
+    }
+
+    return false;
 }
 
 
