4cc6e2005ac8dcbf35e7ad95261c956e0e475e8f
Pranith Kumar <bobby.prani@gmail.com>
2026-01-24 23:57:14 +0000

    cpu-o3: Add per-CPU heartbeat

--- src/cpu/o3/cpu.hh
+++ src/cpu/o3/cpu.hh
@@ -587,6 +587,11 @@ class CPU : public BaseCPU
     /** The cycle that the CPU was last running, used for statistics. */
     Cycles lastRunningCycle;
 
+    /** Per-CPU heartbeat state. */
+    mutable Counter heartbeatPrevTotalInsts = 0;
+    mutable Cycles heartbeatPrevIntervalCycles = Cycles(0);
+    mutable Cycles heartbeatStartCycles = Cycles(0);
+
     /** The cycle that the CPU was last activated by a new thread*/
     Tick lastActivatedCycle;
 
