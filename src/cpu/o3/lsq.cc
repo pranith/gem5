7f017bd1826f616d701e8104fa13737432c3d1cc
Pranith Kumar <bobby.prani@gmail.com>
2026-01-29 07:45:07 -0800

    misc: Store only mark higher versioned loads as violating

--- src/cpu/o3/lsq.cc
+++ src/cpu/o3/lsq.cc
@@ -324,9 +324,15 @@ LSQ::commitStores(InstSeqNum &youngest_inst, ThreadID tid)
 { thread.at(tid)->commitStores(youngest_inst); }
 
 unsigned
-LSQ::markLoadsHitExternalSnoop(ThreadID tid, InstSeqNum barrier_sn)
+LSQ::markLoadsHitExternalSnoopAfter(ThreadID tid, InstSeqNum barrier_sn)
 {
-    return thread.at(tid).markLoadsHitExternalSnoop(barrier_sn);
+    return thread.at(tid).markLoadsHitExternalSnoopAfter(barrier_sn);
+}
+
+unsigned
+LSQ::markLoadsHitExternalSnoop(ThreadID tid, uint64_t version)
+{
+    return thread.at(tid).markLoadsHitExternalSnoop(version);
 }
 
 void
